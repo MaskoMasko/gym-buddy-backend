9e1cde366198338840cba042666dc6444a57be2b
Massimo Persic <persic.massimo@gmail.com>
2023-02-18 00:04:10 +0100

    added password

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -14,63 +14,89 @@ if (!process.env.SENDGRID_API_KEY) {
   );
 }
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const salt = crypto.randomBytes(16).toString("hex");
 
 router.post("/login", async (req, res) => {
-  const { name, email } = req.body;
-  const user = await client.user.findFirst({
-    where: { name, email },
-  });
-  // if else >> if return return !?
-  if (!user) {
-    res.send({ message: "Invalid name or email" });
-  } else if (user && !user.emailVerified) {
-    res.send({ message: "Verify your email before continuing." });
-  } else {
-    res.send({ message: "Login successful." });
-  }
-  console.log({ login: user });
+  const { email, password } = req.body;
+  crypto.pbkdf2(
+    password,
+    salt,
+    10000,
+    64,
+    "sha512",
+    async (err, derivedKey) => {
+      if (err) throw err;
+
+      // compare the derived key with the previously hashed password
+      let hashedPassword = derivedKey.toString("hex");
+      console.log(hashedPassword);
+      const user = await client.user.findFirst({
+        where: { email, password: hashedPassword },
+      });
+      // if else >> if return return !?
+      if (!user) {
+        res.json({ message: "Check your credentials." });
+      } else if (user && !user.emailVerified) {
+        res.json({ message: "Verify your email before continuing." });
+      } else {
+        res.json({ message: "Login successful." });
+      }
+      console.log({ login: user });
+    }
+  );
 });
 
 router.post("/signup", async (req, res) => {
-  const { name, email } = req.body;
+  const { name, email, password } = req.body;
   const verificationToken = jwt.sign({ email }, secretKey, {
     expiresIn: "24h",
   });
-  const user = await client.user.create({
-    data: {
-      name,
-      email,
-      token: verificationToken,
-      friends: { connectOrCreate: [] },
-    },
-  });
-  console.log({ signup: user });
-  if (!user) {
-    throw Error("Error while creating a user. Try again later!");
-  }
-  if (!process.env.EMAIL) {
-    throw Error(
-      "process.env.EMAIL is undefined. Check your env file configuration."
-    );
-  }
-  const msg = {
-    to: process.env.EMAIL, // Change to your recipient
-    from: process.env.EMAIL, // Change to your verified sender
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: `<strong><a href="http://localhost:4000/verify?token=${verificationToken}">Verify email</a></strong>`,
-  };
-  sgMail
-    .send(msg)
-    .then(() => {
-      res.send({
-        message:
-          "User was successfully created. Verify your email to continue!",
+  crypto.pbkdf2(
+    password,
+    salt,
+    10000,
+    64,
+    "sha512",
+    async (err, derivedKey) => {
+      if (err) throw err;
+      let hashedPassword = derivedKey.toString("hex");
+      const user = await client.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+          token: verificationToken,
+        },
       });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+      console.log({ signup: user });
+      if (!user) {
+        throw Error("Error while creating a user. Try again later!");
+      }
+      if (!process.env.EMAIL) {
+        throw Error(
+          "process.env.EMAIL is undefined. Check your env file configuration."
+        );
+      }
+      const msg = {
+        to: process.env.EMAIL, // Change to your recipient
+        from: process.env.EMAIL, // Change to your verified sender
+        subject: "Sending with SendGrid is Fun",
+        text: "and easy to do anywhere, even with Node.js",
+        html: `<strong><a href="http://localhost:4000/verify?token=${verificationToken}">Verify email</a></strong>`,
+      };
+      sgMail
+        .send(msg)
+        .then(() => {
+          res.send({
+            message:
+              "User was successfully created. Verify your email to continue!",
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  );
 });
 
 router.get("/verify", async (req, res) => {
@@ -88,5 +114,44 @@ router.get("/verify", async (req, res) => {
   console.log({ verifiedUser: updatedUser });
   res.send("Email verified");
 });
-
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  const user = await client.user.findFirst({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    throw Error(`Email ${email} doesn't exist in our database!`);
+  }
+  if (!process.env.EMAIL) {
+    throw Error(
+      "process.env.EMAIL is undefined. Check your env file configuration."
+    );
+  }
+  const msg = {
+    to: process.env.EMAIL, // Change to your recipient
+    from: process.env.EMAIL, // Change to your verified sender
+    subject: "Forgot password",
+    text: "forgot password",
+    html: `<strong><a href="http://localhost:4000/password-reset-success">Reset password</a></strong>`,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      res.send({
+        message: "Check email and confirm that you own that account!",
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+router.get("/password-reset-success", (req, res) => {
+  res.send("Success! Return to app and reset password");
+});
 export default router;
+
+//login with invalid email -> create account -> verify email -> access to other routes
+//login with valid email -> access to other routes
+//reset-password has to work only in app
