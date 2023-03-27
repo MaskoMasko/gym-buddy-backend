2b2c86e5b08192d516c39ee2170c2a74c27767e2
Massimo Persic <persic.massimo@gmail.com>
2023-03-27 15:32:39 +0200

    blogs

--- src/server.ts
+++ src/server.ts
@@ -7,18 +7,23 @@ import friends from "./routes/friends";
 import gymLocations from "./routes/gymLocations";
 import home from "./routes/home";
 import messages from "./routes/messages";
+import workouts from "./routes/workouts";
+import blogs from "./routes/blogs";
 
 const app = express();
 app.use(bodyParser.urlencoded());
 app.use(cookieparser());
 
 app.use(bodyParser.json());
+app.use("/images", express.static("images"));
 app.use("/", authRouter);
 app.use("/", home);
 app.use("/", chatRoom);
 app.use("/", messages);
 app.use("/", gymLocations);
 app.use("/", friends);
+app.use("/", workouts);
+app.use("/", blogs);
 
 app.listen(4000, () => {
   console.log(`Server running on http://localhost:4000`);
